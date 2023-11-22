71fe60e7baeab50aaaa89423178fa0b5eda74553
Saymon Araújo <67705166+saymon-araujo@users.noreply.github.com>
2023-11-22 08:47:47 -0300

    feat: add carrousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import { useKeenSlider } from "keen-slider/react";
+
 import { styled } from "../styles";
 import { HomeContainer, Product } from "../styles/pages/home";
-
 import shirt1 from "../assets/shirts/1.png";
 import shirt2 from "../assets/shirts/2.png";
 import shirt3 from "../assets/shirts/3.png";
+
+import "keen-slider/keen-slider.min.css";
 
 const Button = styled("button", {
   color: "$primary",
@@ -20,9 +23,16 @@ const Button = styled("button", {
 });
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={shirt1} alt="" width={520} height={480} />
         <footer>
           <strong>Camiseta X</strong>
@@ -30,11 +40,27 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={shirt2} alt="" width={520} height={480} />
         <footer>
           <strong>Camiseta X2</strong>
-          <span>R$ 79,00</span>
+          <span>R$ 89,00</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta X3</strong>
+          <span>R$ 99,00</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta X3</strong>
+          <span>R$ 99,00</span>
         </footer>
       </Product>
     </HomeContainer>
